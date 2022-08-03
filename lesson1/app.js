3eff3b1fa8905aeab82e40b6b8adeaf65a57b087
Temirlan006 <ahunzanovtemirlan@gmail.com>
2022-08-03 19:38:56 +0600

    lesson1

---
+++ lesson1/app.js
@@ -0,0 +1,41 @@
+const a = "hello";//str
+
+let c = 25;//number
+
+const bool = true; //boolean
+const bool2 =  false;
+
+const arr = ["Aidana", true, false, 34]
+console.log(arr[0])
+
+const obj =  {
+    key: "value",
+    username: 'Sara',
+    data:{
+        first_name: "jack",
+        last:'Rus'
+    },
+    monkey: [
+        {
+            name: 'Ruslan'
+        }
+    ]
+}
+
+console.log(obj.monkey[0].name)
+
+
+// const age = prompt("Сколько вам лет?")
+//
+// if(age < 18){
+//     alert("вам нельзя входить")
+// }else if(age >= 18){
+//     alert("вам можно входить")
+// }else{
+//     console.log("пошел вон ")
+// }
+
+console.log(30 * 40 - 203)
+console.log(-30 * 203)
+console.log(3**4)
+console.log(3 * 20 / 11)
